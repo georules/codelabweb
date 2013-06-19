e2529d954cf08f81def7af5afd848b60fb204131
Geo Miller <geoffery.miller@gmail.com>
2013-06-19 17:00:05 -0400

    more server work

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ GLOBAL.Handlebars = require('handlebars');
 require('./templates/server-templates');
 
 var mongo = require('mongoskin');
+var BSON = mongo.BSONPure;
 var db = mongo.db('localhost:27017/codelabweb');
-var images = db.collection("lab");
+var labs = db.collection("lab");
 var request = require('request');
 
 var express = require('express');
@@ -13,20 +14,19 @@ var app = express()
 	.use('/', express.static(__dirname+'/static'));
 
 app.get("/", function(req,res)	{
-	var template = Handlebars.templates.base;
-	var html = template({file:false});
-	res.send(html);
+	htmlcode = "<h1>Hello World</h1>";
+	csscode = "body {background-color:grey;}";
+	labs.insert({htmlcode:htmlcode,csscode:csscode},{}, function(err, data) {
+		id = data[0]._id;
+		res.redirect('/'+id);
+	});
 });
 
-app.post("/", function(req,res)	{
-	try{
-	var filename = process_data(req);
-	fname = filename.replace(__dirname,"");
-	var template = Handlebars.templates.base;
-	var html = template({file:fname});
-	}
-	catch(e) {console.log(e);}
-	res.send(html);
+app.post("/:id", function(req,res)	{
+	htmlcode = req.body.htmlcode;
+	csscode = req.body.csscode;
+	bid = BSON.ObjectID(req.params.id);
+	labs.update({"_id":bid}, {$set:{htmlcode:htmlcode,csscode:csscode}}, {upsert:true});
 });
 
 app.get("/api", function(req,res) {
@@ -34,6 +34,16 @@ app.get("/api", function(req,res) {
 	info = "post /api/svg requires svgcode parameter"
 	app.routes.post[1].info = info;
 	res.send(app.routes);
+});
+
+app.get("/:id", function (req,res)	{
+	var template = Handlebars.templates.base;
+	var id = req.params.id;
+  console.log(id);
+	labs.findOne({"_id":BSON.ObjectID(id)}, function(err, data) {
+		var html = template({htmlcode:data.htmlcode, csscode:data.csscode, id:id});
+		res.send(html);
+	});
 });
 
 app.listen(settings.port, function() {
